c2ebcf3397ffab61dd6cd5e4c78da7211f099230
caguirre5 <107177715+caguirre5@users.noreply.github.com>
2024-01-10 11:26:56 -0600

    WIP

--- src/pages/ICE/CoffeeProcess.tsx
+++ src/pages/ICE/CoffeeProcess.tsx
@@ -6,6 +6,13 @@ function CoffeeProcess() {
             <div className='title-container'>
                 <p>process</p>
             </div>
+            <div className='intro-text'>
+                <p>From skilled farmers to meticulous roasting specifications, coffee beans go through an amazing journey that brings out their unique flavors. It's a fascinating process that involves many different people and places, and one that we are excited to share with you.</p>
+                <p>let us take you on a journey through the world of coffee!</p>
+            </div>
+            <div className='grid-layout-container'>
+                <p>Guatemala has up to 300 microclimates, each offering unique growing conditions that affect crop development.</p>
+            </div>
         </div>
     )
 }
--- src/pages/ICE/VRExp.tsx
+++ src/pages/ICE/VRExp.tsx
@@ -8,6 +8,14 @@ function VRExperience() {
             <Header/>
             <div className="main">
                 <h1>VR EXPERIENCE</h1>
+                <div>
+                <h1>Tour 360</h1>
+                <iframe
+                    title="3Dvista Tour"
+                    src="https://storage.net-fs.com/hosting/2727323/487/"
+                    allowFullScreen
+                ></iframe>
+                </div>
             </div>
             <Footer/>
         </div>
