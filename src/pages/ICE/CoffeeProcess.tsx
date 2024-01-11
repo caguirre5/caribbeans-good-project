1407cf037038d05be52c9de623656fd413d3d8df
caguirre5 <107177715+caguirre5@users.noreply.github.com>
2024-01-11 17:52:10 -0600

    WIP

--- src/pages/ICE/CoffeeProcess.tsx
+++ src/pages/ICE/CoffeeProcess.tsx
@@ -1,5 +1,14 @@
 import './CoffeeProcess.css'
 
+// GIFS
+import gif1 from '../../assets/GIFS/Grow.an.gif'
+import gif2 from '../../assets/GIFS/Wetmill.ani.gif'
+import gif3 from '../../assets/GIFS/DRYMILL.an.gif'
+import gif4 from '../../assets/GIFS/Export ani.gif'
+
+//Taste Wheel
+import TasteWheelImg from '../../assets/ICE/Tastewheel.png'
+
 function CoffeeProcess() {
     return (
         <div className='page-container-cp'>
@@ -10,8 +19,40 @@ function CoffeeProcess() {
                 <p>From skilled farmers to meticulous roasting specifications, coffee beans go through an amazing journey that brings out their unique flavors. It's a fascinating process that involves many different people and places, and one that we are excited to share with you.</p>
                 <p>let us take you on a journey through the world of coffee!</p>
             </div>
-            <div className='grid-layout-container'>
+            <div className='grid-layout-container gl1'>
                 <p>Guatemala has up to 300 microclimates, each offering unique growing conditions that affect crop development.</p>
+            </div>
+            <div className='grid-layout-container gl2'>
+                <p>Coffee was first introduced to Guatemala in the mid-1700s, but it was not until 1860 that production seriously took off. Prior to that, the plant was only used for ornamental purposes. As the carmine dye industry began to decline, which was the biggest industry in Guatemala at that point, the government searched for new industries to improve the economy.</p>
+            </div>
+            <div className='gif gif1'>
+                <img src={gif1} alt="" />
+                <p>Coffee is harvested once or twice a year, depending on whether it is sun-grown or shade-grown. Guatemala grows coffee under natural shade, using either native species or specific trees. As a result, Guatemala only harvests coffee once a year, making every crop precious to the farmers and their families.</p>
+            </div>
+            <div className='gif gif2'>
+                <p>When it comes to processing coffee, farmers have a few different options to choose from. Each of these processes can yield unique flavors, so it's important for farmers to carefully consider which one they want to use. One popular option is the washed process, which involves pulping the coffee cherries, washing them, and then drying them. Another option is the honey process, which involves pulping the cherries but skipping the washing step. Finally, there's the natural process, in which the cherries are simply dried without any processing. While each of these methods has its own pros and cons, ultimately it's up to the farmer to decide which one will produce the best coffee for their needs.</p>
+                <img src={gif2} alt="" />
+            </div>
+            <div className='gif gif3'>
+                <img src={gif3} alt="" />
+                <div>
+                    <p>During the Dry Mill process, coffee beans undergo several steps:</p>
+                    <ul>
+                        <li>A magnet is used to remove any metal or foreign objects.</li>
+                        <li>A destoner is used to get rid of rocks.</li>
+                        <li>The beans are then polished to remove an outer layer called parchment.</li>
+                        <li>The coffee beans are graded based on size, weight, and color.</li>
+                        <li>The last step in the dry mill is a hand-picking process to check for defects and imperfections.</li>
+                    </ul>
+                </div>
+            </div>
+            <div className='gif gif4'>
+                <p>Farmers send their prepared coffees to a unit of our choosing. We do all the paperwork to export them out of Guatemala and import them to the UK. We then store them in the UK and distribute them to local coffee roasters.</p>
+                <img src={gif4} alt="" />
+            </div>
+            <div className='taste-wheel-section shapedividers_com-8743 '>
+                <h1>Taste wheel</h1>
+                <img src={TasteWheelImg} alt="" />
             </div>
         </div>
     )
--- src/pages/ICE/VRExp.tsx
+++ src/pages/ICE/VRExp.tsx
@@ -9,7 +9,6 @@ function VRExperience() {
             <div className="main">
                 <h1>VR EXPERIENCE</h1>
                 <div>
-                <h1>Tour 360</h1>
                 <iframe
                     title="3Dvista Tour"
                     src="https://storage.net-fs.com/hosting/2727323/487/"
