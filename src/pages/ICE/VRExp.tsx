c9e1a39b0b2d67d12edd2c8ddfaa6e89adb7bb93
caguirre5 <107177715+caguirre5@users.noreply.github.com>
2024-03-11 15:52:41 -0600

    Cards

--- src/pages/ICE/VRExp.tsx
+++ src/pages/ICE/VRExp.tsx
@@ -2,6 +2,11 @@ import Header from "../../components/HeaderControls"
 import Footer from "../../components/Footer"
 import './VRExp.css'
 
+import Farm1 from '../../assets/ICE/FarmNames/Amatillo.png'
+import Farm2 from '../../assets/ICE/FarmNames/Hunbatz.png'
+import Farm3 from '../../assets/ICE/FarmNames/San José Ocaña.png'
+import Farm4 from '../../assets/ICE/FarmNames/El Socorro.png'
+
 function VRExperience() {
     return (
         <div className="vr-page-container">
@@ -9,11 +14,17 @@ function VRExperience() {
             <div className="main">
                 <h1>VR EXPERIENCE</h1>
                 <div>
-                <iframe
+                {/* <iframe
                     title="3Dvista Tour"
                     src="https://caribbeangoodsvr.online/"
                     allowFullScreen
-                ></iframe>
+                ></iframe> */}
+                </div>
+                <div className="grid-cards">
+                    <a><img className='card' src={Farm1} alt="" /></a>
+                    <a href="https://caribbeangoodsvr.online/"><img className='card' src={Farm2} alt="" /></a>
+                    <a><img className='card' src={Farm3} alt="" /></a>
+                    <a><img className='card' src={Farm4} alt="" /></a>
                 </div>
             </div>
             <Footer/>
