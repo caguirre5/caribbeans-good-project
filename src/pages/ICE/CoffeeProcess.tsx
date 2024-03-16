0eca2117ca85c105002e3048fe47800ff4b58aef
Cristian Aguirre <107177715+caguirre5@users.noreply.github.com>
2024-03-15 20:59:52 -0600

    WIP

--- src/pages/ICE/CoffeeProcess.tsx
+++ src/pages/ICE/CoffeeProcess.tsx
@@ -6,6 +6,31 @@ import gif2 from '../../assets/GIFS/Wetmill.ani.gif'
 import gif3 from '../../assets/GIFS/DRYMILL.an.gif'
 import gif4 from '../../assets/GIFS/Export ani.gif'
 
+//Mural Arts
+import Art1 from '../../assets/ICE/CoffeeProcessMural/Artboard1.png'
+import Art2 from '../../assets/ICE/CoffeeProcessMural/Artboard2.png'
+import Art3 from '../../assets/ICE/CoffeeProcessMural/Artboard3.png'
+import Art4 from '../../assets/ICE/CoffeeProcessMural/Artboard4.png'
+import Art5 from '../../assets/ICE/CoffeeProcessMural/Artboard5.png'
+import Art6 from '../../assets/ICE/CoffeeProcessMural/Artboard6.png'
+import Art7 from '../../assets/ICE/CoffeeProcessMural/Artboard7.png'
+import Art8 from '../../assets/ICE/CoffeeProcessMural/Artboard8.png'
+import Art9 from '../../assets/ICE/CoffeeProcessMural/Artboard9.png'
+import Art10 from '../../assets/ICE/CoffeeProcessMural/Artboard10.png'
+import Art11 from '../../assets/ICE/CoffeeProcessMural/Artboard11.png'
+import Art12 from '../../assets/ICE/CoffeeProcessMural/Artboard12.png'
+import Art13 from '../../assets/ICE/CoffeeProcessMural/Artboard13.png'
+import Art14 from '../../assets/ICE/CoffeeProcessMural/Artboard14.png'
+import Art15 from '../../assets/ICE/CoffeeProcessMural/Artboard15.png'
+import Art16 from '../../assets/ICE/CoffeeProcessMural/Artboard16.png'
+import Art17 from '../../assets/ICE/CoffeeProcessMural/Artboard17.png'
+import Art18 from '../../assets/ICE/CoffeeProcessMural/Artboard18.png'
+import Art19 from '../../assets/ICE/CoffeeProcessMural/Artboard19.png'
+import Art20 from '../../assets/ICE/CoffeeProcessMural/Artboard20.png'
+import Art21 from '../../assets/ICE/CoffeeProcessMural/Artboard21.png'
+import Art22 from '../../assets/ICE/CoffeeProcessMural/Artboard22.png'
+import Art23 from '../../assets/ICE/CoffeeProcessMural/Artboard23.png'
+
 //Taste Wheel
 import TasteWheelImg from '../../assets/ICE/Tastewheel.png'
 
@@ -33,6 +58,77 @@ function CoffeeProcess() {
             <BouncingDiv className='grid-layout-container gl2'>
                 <p>Coffee was first introduced to Guatemala in the mid-1700s, but it was not until 1860 that production seriously took off. Prior to that, the plant was only used for ornamental purposes. As the carmine dye industry began to decline, which was the biggest industry in Guatemala at that point, the government searched for new industries to improve the economy.</p>
             </BouncingDiv>
+            <div className='mural'>
+                <div className='mural-item LMural'>
+                    <img src={Art1} alt="" />
+                </div>
+                <div className='mural-item RMural'>
+                    <img src={Art2} alt="" />
+                </div>
+                <div className='mural-item LMural'>
+                    <img src={Art3} alt="" />
+                </div>
+                <div className='mural-item RMural'>
+                    <img src={Art4} alt="" />
+                </div>
+                <div className='mural-item LMural'>
+                    <img src={Art5} alt="" />
+                </div>
+                <div className='mural-item RMural'>
+                    <img src={Art6} alt="" />
+                </div>
+                <div className='mural-item LMural'>
+                    <img src={Art7} alt="" />
+                </div>
+                <div className='mural-item RMural'>
+                    <img src={Art8} alt="" />
+                </div>
+                <div className='mural-item LMural'>
+                    <img src={Art9} alt="" />
+                </div>
+                <div className='mural-item RMural'>
+                    <img src={Art10} alt="" />
+                </div>
+                <div className='mural-item LMural'>
+                    <img src={Art11} alt="" />
+                </div>
+                <div className='mural-item RMural'>
+                    <img src={Art12} alt="" />
+                </div>
+                <div className='mural-item LMural'>
+                    <img src={Art13} alt="" />
+                </div>
+                <div className='mural-item RMural'>
+                    <img src={Art14} alt="" />
+                </div>
+                <div className='mural-item LMural'>
+                    <img src={Art15} alt="" />
+                </div>
+                <div className='mural-item RMural'>
+                    <img src={Art16} alt="" />
+                </div>
+                <div className='mural-item LMural'>
+                    <img src={Art17} alt="" />
+                </div>
+                <div className='mural-item RMural'>
+                    <img src={Art18} alt="" />
+                </div>
+                <div className='mural-item LMural'>
+                    <img src={Art19} alt="" />
+                </div>
+                <div className='mural-item RMural'>
+                    <img src={Art20} alt="" />
+                </div>
+                <div className='mural-item LMural'>
+                    <img src={Art21} alt="" />
+                </div>
+                <div className='mural-item RMural'>
+                    <img src={Art22} alt="" />
+                </div>
+                <div className='mural-item LMural'>
+                    <img src={Art23} alt="" />
+                </div>
+            </div>
             <div className='gif gif1'>
                 <img src={gif1} alt="" />
                 <SlideDiv className='' variantSide={false}>
